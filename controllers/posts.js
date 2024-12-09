f14b99cce3a2c471729dc8725deca2a0faaec651
Debo <deboogunnowo@gmail.com>
2024-12-10 00:13:12 +0100

    Implementing edit post feature

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,7 +16,7 @@ const renderHome = async (req, res) => {
 
   if (posts.length === 0) {
     // add a page to show no posts yet later
-    return res.status(200).send('<h1> Nothing added yet </h1>');
+    return res.render('pages/homeEmpty');
   }
 
   res.render('pages/home', { posts });
@@ -30,7 +30,7 @@ const renderNewPost = (req, res) => {
   res.render('pages/newPost');
 };
 
-const getPosts = async (req, res) => {
+/* const getPosts = async (req, res) => {
   try {
     const userId = req.user.id;
 
@@ -42,7 +42,7 @@ const getPosts = async (req, res) => {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-};
+}; */
 
 const createPost = async (req, res) => {
   try {
@@ -50,20 +50,48 @@ const createPost = async (req, res) => {
 
     const {title, content} = req.body;
 
-    const newPost = await new Post({title, content, userId});
+    const newPost = new Post({title, content, userId});
     await newPost.save();
+
+    console.log(newPost);
+    res.redirect('/api/posts');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    return res.status(500).send('<h1> Internal server error </h1>');
   }
 };
 
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) 
+      res.status(404).send('<h1> Post not found </h1>');
+
+    return res.render('pages/editPost', {post});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('<h1> Internal server error </h1>');
+  }
+}
+
 const updatePost = async (req, res) => {
-  const userId = req.user.id;
-  const postId = req.params.id;
+  const {title, content} = req.body;
 
-  const updatedPost = await Post.findByIdAndUpdate({_id: postId}, {userId: userId}, req.body);
+  try {
+    const userId = req.user.id;
+    const postId = req.params.id;
+  
+    const updatedPost = await Post.findByIdAndUpdate(postId, {title, content}, {new: true});
+    if (!updatedPost) 
+      res.status(404).send('<h1> Post not found </h1>');
 
+    console.log(updatedPost);
+    res.redirect('/api/posts');
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('<h1> Internal server error </h1>');
+  }
 };
 
 const deletePost = async (req, res) => {
@@ -72,5 +100,5 @@ const deletePost = async (req, res) => {
   const updatedPost = await Post.findByIdAndDelete({_id: postId}, {userId: userId}, req.body);
 };
 
-module.exports = { renderHome, renderNewPost, createPost, getPosts, updatePost, deletePost };
+module.exports = { renderHome, renderNewPost, createPost, getPost, updatePost, deletePost };
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { renderHome, renderNewPost, createPost, getPosts, updatePost, deletePost } = require('../controllers/posts');
+const { renderHome, renderNewPost, createPost, getPost, updatePost, deletePost } = require('../controllers/posts');
 
-router.route('/posts').get(renderHome).get(getPosts).post(createPost);
+router.route('/posts').get(renderHome).post(createPost);
 router.route('/posts/new').get(renderNewPost).post(createPost);
-router.route('/posts/:id').patch(updatePost).delete(deletePost);
+router.route('/posts/:id/edit').get(getPost);
+router.route('/posts/:id').post(updatePost).delete(deletePost);
 
 module.exports = router
