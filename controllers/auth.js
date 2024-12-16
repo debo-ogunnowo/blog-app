444ef1763d952d65b43043996de04d6a68f8f48b
Debo <deboogunnowo@gmail.com>
2024-12-16 14:13:01 +0100

    Final changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,16 +11,26 @@ const renderSignup = (req, res) => {
 }
 
 const createUser = async (req, res) => {
-  const {username, password} = req.body;
+  try {
+    const {username, password} = req.body;
 
-  const user = new User({username, password});
+    if (!User.findOne({username})) {
+      const user = new User({username, password});
 
-  await user.save();
+      await user.save();
 
-  const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET);
-  res.cookie('token', token, {httpOnly: true});
-  console.log(`User created: ${user.username}`, token);
-  res.redirect('/api/posts');
+      const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME});
+      res.cookie('token', token, {httpOnly: true});
+      console.log(`User created: ${user.username}`, token);
+      res.redirect('/api/posts');
+    }
+    else {
+      return res.status(401).send('<h1> Username already exists </h1>');
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('<h1> Internal server error </h1>');
+  }
 };
 
 const loginUser = async (req, res) => {
@@ -28,22 +38,22 @@ const loginUser = async (req, res) => {
     const {username, password} = req.body;
 
     if (!username || !password) {
-      return res.status(400).send('Username and password required');
+      return res.status(400).send('<h1> Username and password required </h1>');
     }
 
     const user = await User.findOne({username});
 
     if (!user){
-      return res.status(401).send('Incorrect username or passwrod');
+      return res.status(401).send('<h1> Incorrect username or password </h1>');
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
 
     if (!validPassword){
-      return res.status(401).send('Incorrect username or passwrod');
+      return res.status(401).send('<h1> Incorrect username or password </h1>');
     }
 
-    const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET);
+    const token = await jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME});
     res.cookie('token', token, {httpOnly: true});
     res.redirect('/api/posts');
   } catch (error) {
